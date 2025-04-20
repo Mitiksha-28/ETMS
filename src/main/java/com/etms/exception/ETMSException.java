5b459d4386fbfeae68614069aaa57a71dc373a85
Manasvi Pawa <manasvi.pawa@gmail.com>
2025-04-20 18:44:31 +0530

    Update ETMSException.java

--- src/main/java/com/etms/exception/ETMSException.java
+++ src/main/java/com/etms/exception/ETMSException.java
@@ -1,20 +1,12 @@
 package com.etms.exception;
 
-/**
- * This class is used to handle application-specific exceptions.
- */
+
 public class ETMSException extends Exception {
 
-    /**
-     * Constructs a new ETMSException with the specified detail message.
-     */
     public ETMSException(String message) {
         super(message);
     }
 
-    /**
-     * Constructs a new ETMSException with the specified detail message and cause.
-     */
     public ETMSException(String message, Throwable cause) {
         super(message, cause);
     }
